62f0d8df7b43c3d5632b5639db4f4769f4d3815c
Hunter Lacefield <lace1010@gmail.com>
2021-03-28 19:42:56 +0800

    removed old files

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-const cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
